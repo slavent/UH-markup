fa23f6aa50172ff92f6e4f66a0c4ec46b60439ed
Slavent <startupne@gmail.com>
2015-01-19 15:05:11 +0300

    slider refactor

--- js/libs/slav_slider.js
+++ js/libs/slav_slider.js
@@ -2,7 +2,6 @@
 	
 	var def_options = {
 		speed: 500,
-		time: 3000,
 		auto: true,
 		nav: true,
 		nav_dots: true,
@@ -13,26 +12,24 @@
 
 		container = $(".slav_slider"),
 		elems = container.find("img"),
-		item_html = "<div class='slav_slider__item'></div>",
-		nav_html = "<div class='slav_slider__nav'></div>",
-		nav_dot_html = "<div class='slav_slider__nav_dot'></div>", 
 
-		nav = {
-			btns: container.find(".nav__btn"),
-			btn_prev: container.find(".nav__btn_prev"),
-			btn_next: container.find(".nav__btn_next"),
-		},
+		html_wrp = "<div class='slav_slider__wrp'></div>",
+		html_item = "<div class='slav_slider__item'></div>",
+		html_nav = "<div class='slav_slider__nav'><div class='nav__btn nav__btn_prev'></div><div class='nav__btn nav__btn_next'></div></div>",
+		html_nav_dot = "<div class='slav_slider__nav_dot'><div class='dot_wrp'></div></div>",
+		html_dot = "<div class='dot'></div>", 
+
+		classCurrent = "current",
+		classNavBtn = "nav__btn",
+		classDotWrp = "dot_wrp",
+		classDot = "dot",
 
 		el = {
 			number: elems.length,
-			width: container.width(),
+			width: elems.width(),
 			height: container.height(),
 			index: 0
 		};
-
-		// console info
-		console.log(el);
-		console.log(nav);
 
 	$.fn.slav_slider = 	function(params){
 
@@ -42,27 +39,40 @@
 		var init = function(){
 
 			var bindEvents = function(){
-				nav.btns.on("click", play);
-				container.on("mousever", stop);
-				container.on("mouseout", play);
+				//container.on("mousever", stop);
+				//container.on("mouseout", play);
 			};
 
 			var makeItemList = function(){
+				container.wrap(html_wrp);
+				container_wrp = container.parent();
+
 				elems.each(function(){
-					$(this).wrap(item_html);
+					$(this).wrap(html_item);
 				});
+
+				container.children().eq(0).addClass(classCurrent);
 			};
 
 			var makeNav = function(){
-				container_wrp.append(nav_html);
+				container_wrp
+					.append(html_nav)
+					.find("." + classNavBtn).on("click", play);
 			};
 
 			var makeDotNav = function(){
-				container_wrp.append(nav_dot_html);
+				container_wrp.append(html_nav_dot);
+
+
+				for(var i = 0; i < elems.length; i++)
+					$("." + classDotWrp).append(html_dot);
+
+				container_wrp.find("." + classDot).eq(0).addClass(classCurrent);
+				container_wrp.find("." + classDot).on("click", play);
 			};
 
 			var makeDescription = function(){
-				var elems = $(".slav_slider__item");
+				var elems = container.children();
 
 				elems.each(function(){
 					var desc = $(this).find("img").data("desc");
@@ -70,9 +80,6 @@
 					$(this).append("<p>" + desc + "</p>");
 				});
 			};
-
-			container.wrap("<div class='slav_slider__wrp'></div>");
-			container_wrp = container.find(".slav_slider__wrp");
 
 			bindEvents();
 			makeItemList();
@@ -85,11 +92,46 @@
 
 		var play = function(){
 
-			el_index = container.find(".current");
+			var container_mLeft = parseInt(container.css("marginLeft")),
+				el_index = container.find("." + classCurrent).index(),
+				width = el.width;
 
-			var direction = function(){
+			var stateClass = function(dir){
+				if(dir == "prev") var ind = el_index - 1;
+				if(dir == "next") var ind = el_index + 1;
+				if(!isNaN(dir)) var ind = dir;
 
+				container.children().removeClass(classCurrent);
+				container.children().eq(ind).addClass(classCurrent);
+
+				container_wrp.find("." + classDot).removeClass(classCurrent);
+				container_wrp.find("." + classDot).eq(ind).addClass(classCurrent);
 			};
+
+			if($(this).hasClass("dot")){
+
+				var i = $(this).index();
+
+				container.animate({"marginLeft": -width * i}, options.speed);
+				stateClass(i);
+
+			} else {
+
+				if($(this).hasClass("nav__btn_prev")){
+					if(el_index == 0) return false;
+					stateClass("prev");
+					width = width;
+				}
+
+				if($(this).hasClass("nav__btn_next")){
+					if(el_index == elems.length - 1) return false;
+					stateClass("next");
+					width = -width;
+				}
+
+				container.animate({"marginLeft": container_mLeft + width}, options.speed);
+
+			}
 
 		};
 
