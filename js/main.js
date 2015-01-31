3d4e96e5cc99578e033eb650361a7349d5b815ef
Slavent <startupne@gmail.com>
2015-01-31 20:53:20 +0300

    finish markup

--- js/main.js
+++ js/main.js
@@ -1,1 +1,10 @@
-
+$(function(){
+	$(".col__nav_header li").hover(
+		function(){
+			$(this).find(".col__hover").stop(true).fadeIn();
+		},
+		function(){
+			$(this).find(".col__hover").stop(true).fadeOut();
+		}
+	);
+});
