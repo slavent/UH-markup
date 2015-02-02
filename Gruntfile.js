4b93957624bb0eec112a2b6eadc1e943696f69f7
Slavent <startupne@gmail.com>
2015-02-02 19:18:01 +0300

    add Grunt tasks

---
+++ Gruntfile.js
@@ -0,0 +1,30 @@
+module.exports = function(grunt) {
+
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+
+        concat: {
+            dist: {
+                src: [
+                    'js/libs/*.js',
+                    'js/main.js'
+                ],
+                dest: 'js/build/production.js',
+            }
+        },
+
+        uglify: {
+            build: {
+                src: 'js/build/production.js',
+                dest: 'js/build/production.min.js'
+            }
+        }
+
+    });
+
+    grunt.loadNpmTasks('grunt-contrib-concat');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+
+    grunt.registerTask('default', ['concat', 'uglify']);
+
+};
